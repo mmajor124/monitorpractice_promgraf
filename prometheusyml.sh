b56415b50ae1d62c4828e18a918514f01fa2dbb2
Mike Major <75327032+mmajor124@users.noreply.github.com>
2024-08-13 01:32:25 -0400

    Update prometheusyml.sh

--- prometheusyml.sh
+++ prometheusyml.sh
@@ -1,1 +1,16 @@
+global:
+  scrape_interval: 5s
+  evaluation_interval: 15s
 
+scrape_configs:
+  - job_name: 'http_traffic_generator'
+    static_configs:
+      - targets: ['ec2publicip:4000']
+
+  - job_name: 'prometheus'
+    static_configs:
+      - targets: ['localhost:9090']
+
+  - job_name: 'node_exporter'
+    static_configs:
+      - targets: ['ec2publicip:9100']
